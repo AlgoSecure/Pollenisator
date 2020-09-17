2a896b10083e77a2d0a8187f841bc83c4c53e214
Your Name <you@example.com>
2020-09-17 16:34:48 +0200

    added notify to modules

--- core/Application/Appli.py
+++ core/Application/Appli.py
@@ -408,6 +408,11 @@ class Appli(ttk.Frame):
                 else:
                     self.treevw.notify(notification["db"], notification["collection"],
                                     notification["iid"], notification["action"], notification.get("parent", ""))
+                    for module in self.modules:
+                        if callable(getattr(module["object"], "notify", None)):
+                            module["object"].notify(notification["db"], notification["collection"],
+                                    notification["iid"], notification["action"], notification.get("parent", ""))
+
         except Exception as e:
             print(str(e))
         self.notifications_timers = threading.Timer(
